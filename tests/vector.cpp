b8f75c08a318d3df6756e1e1ba8978f16892a2a9
Aduersarius <31540232+Aduersarius@users.noreply.github.com>
2018-06-10 14:40:17 +0300

    Update vector.cpp

--- tests/vector.cpp
+++ tests/vector.cpp
@@ -6,8 +6,8 @@
 TEST_CASE("creating vector")
 {
 	vector_t vector;
-	REQUIRE( vector.size() == 0 );
-	REQUIRE( vector.capacity() == 0 );
+	REQUIRE( vector.size_() == 0 );
+	REQUIRE( vector.capacity_() == 0 );
 }
 
 TEST_CASE("copying vector")
@@ -62,24 +62,24 @@ TEST_CASE("pushing elements")
 	vector_t vector;
 
 	vector.push_back(1);
-	REQUIRE( vector.size() == 1 );
-	REQUIRE( vector.capacity() == 1 );
+	REQUIRE( vector.size_() == 1 );
+	REQUIRE( vector.capacity_() == 1 );
 
 	vector.push_back(2);
-	REQUIRE( vector.size() == 2 );
-	REQUIRE( vector.capacity() == 2 );
+	REQUIRE( vector.size_() == 2 );
+	REQUIRE( vector.capacity_() == 2 );
 
 	vector.push_back(3);
 	REQUIRE( vector.size() == 3 );
 	REQUIRE( vector.capacity() == 4 );
 
 	vector.push_back(4);
-	REQUIRE( vector.size() == 4 );
-	REQUIRE( vector.capacity() == 4 );
+	REQUIRE( vector.size_() == 4 );
+	REQUIRE( vector.capacity_() == 4 );
 
 	vector.push_back(5);
-	REQUIRE( vector.size() == 5 );
-	REQUIRE( vector.capacity() == 8 );
+	REQUIRE( vector.size_() == 5 );
+	REQUIRE( vector.capacity_() == 8 );
 }
 
 TEST_CASE("poping elements")
@@ -94,26 +94,26 @@ TEST_CASE("poping elements")
 	vector.push_back(6);
 
 	vector.pop_back();
-	REQUIRE( vector.size() == 5 );
-	REQUIRE( vector.capacity() == 8 );
+	REQUIRE( vector.size_() == 5 );
+	REQUIRE( vector.capacity_() == 8 );
 
 	vector.pop_back();
-	REQUIRE( vector.size() == 4 );
-	REQUIRE( vector.capacity() == 8 );
+	REQUIRE( vector.size_() == 4 );
+	REQUIRE( vector.capacity_() == 8 );
 
 	vector.pop_back();
-	REQUIRE( vector.size() == 3 );
-	REQUIRE( vector.capacity() == 8 );
+	REQUIRE( vector.size_() == 3 );
+	REQUIRE( vector.capacity_() == 8 );
 
 	vector.pop_back();
-	REQUIRE( vector.size() == 2 );
-	REQUIRE( vector.capacity() == 4 );
+	REQUIRE( vector.size_() == 2 );
+	REQUIRE( vector.capacity_() == 4 );
 
 	vector.pop_back();
-	REQUIRE( vector.size() == 1 );
-	REQUIRE( vector.capacity() == 2 );
+	REQUIRE( vector.size_() == 1 );
+	REQUIRE( vector.capacity_() == 2 );
 
 	vector.pop_back();
-	REQUIRE( vector.size() == 0 );
-	REQUIRE( vector.capacity() == 1 );
+	REQUIRE( vector.size_() == 0 );
+	REQUIRE( vector.capacity_() == 1 );
 }
